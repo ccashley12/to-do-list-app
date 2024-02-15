076e18770f5b5db2f1fa6340fadd1ee82c9bebac
ccashley12 <ccashley12@gmail.com>
2024-02-15 17:08:44 -0500

    corrected JQuery code to add a variable

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function newItem(){
 
 //1. Adding a new item to the list of items: 
     let li = $('<li></li>');
-    let inputValue = ('#input').val();
+    let inputValue = $('#input').val();
     li.append(inputValue);
     
     if (inputValue === '') {
